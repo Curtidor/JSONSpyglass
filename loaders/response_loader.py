ce1373738bb0e398cb9789622cd02f639d46e469
Curtidor <tannermatos18@gmail.com>
2024-02-03 16:20:02 -0500

    add support for retrying urls and refactor ResponseLoader class from static methods to instance methods and members

--- loaders/response_loader.py
+++ loaders/response_loader.py
@@ -3,13 +3,13 @@ import logging
 import httpx
 
 from enum import Enum
-from typing import Coroutine, Dict, AsyncGenerator, List, Set, Tuple, Generator, Any
+from typing import Coroutine, Dict, AsyncGenerator, List, Set, Tuple, Iterable
 from urllib.parse import urlsplit, urlunsplit, urljoin, urlparse
 from playwright.async_api import Page, Request, Locator
 from selectolax.parser import HTMLParser
 from EVNTDispatch import EventDispatcher, PEvent, EventType
 
-from scraping.proxy_manager import ProxyManager
+from scraping.proxy_manager import ProxyManager, Proxy
 from scraping.page_manager import BrowserManager
 from utils.clogger import CLogger
 
@@ -49,38 +49,42 @@ class ResponseLoader:
     """
     A utility class for loading and processing web responses.
     """
-    _max_responses = 60
-    _max_renders = 5
-    _event_dispatcher: EventDispatcher = None
+    _HREF_VALUES_TO_CLICK = {'#', 'javascript:void(0);', 'javascript:;'}
 
-    _response_semaphore = asyncio.Semaphore(_max_responses)
-    _render_semaphore = asyncio.Semaphore(_max_renders)
+    def __init__(self, event_dispatcher: EventDispatcher, use_proxies: bool = False, render: bool = False, max_proxies: int = 30, max_retries: int = 3):
+        # max responses to load at once
+        self.max_responses = 60
+        # max pages to render at once
+        self.max_renders = 5
+        self.use_proxies = use_proxies
+        self.max_proxies = max_proxies
+        self.render = render
+        self.max_retires = max_retries
 
-    _hrefs_values_to_click = {'#', 'javascript:void(0);', 'javascript:;'}
+        # noinspection PyTypeChecker
+        self._current_proxy: Proxy = None
+        self._event_dispatcher = event_dispatcher
+        self._response_semaphore = asyncio.Semaphore(self.max_responses)
+        self._render_semaphore = asyncio.Semaphore(self.max_renders)
+        self._is_initialized = False
+        self._logger = CLogger("ResponseLoader", logging.INFO, {logging.StreamHandler(): logging.INFO})
+        self._proxy_manager = ProxyManager()
 
-    _is_initialized: bool = False
+        # Dict[url: times_retired]
+        self._urls_to_retry: Dict[str, int] = {}
 
-    _proxy_manager = ProxyManager()
-    _logger = CLogger("ResponseLoader", logging.INFO, {logging.StreamHandler(): logging.INFO})
+    async def setup(self) -> None:
+        if self._is_initialized:
+            return
 
-    @classmethod
-    async def setup(cls, event_dispatcher: EventDispatcher, use_proxies: bool = False, max_proxies: int = 30) -> None:
-        cls._event_dispatcher = event_dispatcher
+        self._is_initialized = True
 
-        if use_proxies:
-            await cls._proxy_manager.load_proxies(max_proxies)
+        if self.use_proxies:
+            await self._proxy_manager.load_proxies(self.max_proxies)
+            self._current_proxy = self._proxy_manager.get_random_proxy()
 
     @staticmethod
     def normalize_url(url: str) -> str:
-        """
-        Normalize a URL.
-
-        Args:
-            url (str): The URL to be normalized.
-
-        Returns:
-            str: The normalized URL.
-        """
         components = urlsplit(url)
         normalized_components = [
             components.scheme.lower(),
@@ -92,47 +96,24 @@ class ResponseLoader:
         normalized_url = urlunsplit(normalized_components)
         return normalized_url
 
-    @classmethod
-    async def wait_for_page_load(cls, page: Page, timeout_time: float = 30) -> None:
+    async def wait_for_page_load(self, page: Page, timeout_time: float = 30) -> None:
         try:
             await asyncio.wait_for(
                 asyncio.gather(
-                    # ERROR: the load event is creating time out errors
                     page.wait_for_load_state("load", timeout=timeout_time),
                     page.wait_for_load_state("networkidle", timeout=timeout_time),
                 ),
                 timeout=timeout_time / 1000  # Convert back to seconds
             )
         except asyncio.TimeoutError as te:
-            cls._logger.error(
+            self._logger.error(
                 f"TIME OUT ERROR WHEN WAITING FOR [load state]: {te}\n URL: {page.url}",
             )
 
-    @classmethod
-    async def get_rendered_response(cls, url: str, timeout_time: float = 30,
-                                    use_proxies: bool = False) -> ScrapedResponse:
-        """
-        Get the rendered HTML response content of a web page.
-
-        Args:
-        :param url: The URL of the web page.
-        :param timeout_time: Maximum operation time in seconds (default is 30 seconds).
-        :param use_proxies: Should the request be made using a proxies
-
-
-        Returns:
-          ScrapedResponse: A response object containing the rendered HTML content and additional information.
-
-        Note:
-          If href elements are returned, the page is not closed, and the caller is responsible for managing
-          the page's lifetime.
-        """
-        if use_proxies:
-            raise NotImplementedError('proxies are not supported when making render request')
-
+    async def get_rendered_response(self, url: str, timeout_time: float = 30) -> ScrapedResponse:
         timeout_time *= 1000
 
-        async with cls._render_semaphore:
+        async with self._render_semaphore:
             page = await BrowserManager.get_page()
 
             response = await page.goto(url, timeout=timeout_time)
@@ -145,96 +126,56 @@ class ResponseLoader:
 
             page.on("requestfinished", request_finished_callback)
 
-            await cls.wait_for_page_load(page, timeout_time)
+            await self.wait_for_page_load(page, timeout_time)
 
             html = ""
             try:
-                # Wait for the content_future with a timeout
                 html = await asyncio.wait_for(content_future, timeout=timeout_time / 1000)
             except asyncio.TimeoutError as te:
-                cls._logger.error(
+                self._logger.error(
                     f"TIME OUT ERROR WHEN WAITING FOR [request finished] event: {te}\n (T-O-E) URL: {url}"
                 )
             finally:
                 page.remove_listener("requestfinished", request_finished_callback)
 
-            hrefs_elements = await cls.collect_hrefs_with_elements(page)
+            hrefs_elements = await self.collect_null_hrefs(page)
 
-            # fallback when we couldn't render the page and extract the html
             if not html:
-                cls._logger.warning("Failed to fetch html, falling back to safety fetch")
+                self._logger.warning("Failed to fetch html, falling back to safety fetch")
                 html = await page.content()
 
             status_code = response.status if response else 400
             return ScrapedResponse(html, status_code, href_elements=hrefs_elements, page=page, url=url)
 
-    @classmethod
-    async def get_response(cls, url: str, timeout_time: float = 30, use_proxies: bool = False) -> ScrapedResponse:
-        """
-        Get the text response content of a web page.
-
-        Args:
-        :param url: The URL of the web page.
-        :param timeout_time: Maximum operation time in seconds (default is 30 seconds).
-        :param use_proxies: Should the request be made using a proxies
-
-        Returns:
-            str: The text response content.
-        """
-        async with cls._response_semaphore:
-            if not use_proxies:
+    async def get_response(self, url: str, timeout_time: float = 30) -> ScrapedResponse:
+        async with self._response_semaphore:
+            if not self.use_proxies:
                 async with httpx.AsyncClient(timeout=timeout_time) as client:
                     response = await client.get(url=url)
                     html = response.text
-
-                    return ScrapedResponse(html, response.status_code, url=url)
             else:
-                proxy = cls._proxy_manager.get_random_proxy()
-                formatted_proxy = f'{proxy.protocol}://{proxy.ip}:{proxy.port}'
+                formatted_proxy = f'{self._current_proxy.protocol}://{self._current_proxy.ip}:{self._current_proxy.port}'
 
-                url = url.replace('https', 'http', 1) if url.startswith('https') else url
+                if self._current_proxy.protocol == 'http':
+                    url = url.replace('https', 'http', 1) if url.startswith('https') else url
 
                 async with httpx.AsyncClient(proxies=formatted_proxy, timeout=timeout_time) as client:
                     response = await client.get(url=url)
                     html = response.text
 
-                    return ScrapedResponse(html, response.status_code, url=url)
+            return ScrapedResponse(html, response.status_code, url=url)
 
-    @classmethod
-    async def load_responses(cls, urls: Set[str], render_pages: bool = False, use_proxies: bool = False) \
-            -> Dict[str, ScrapedResponse]:
-        """
-        Load and retrieve responses from the specified URLs.
+    async def load_responses(self, urls: Set[str]) -> Dict[str, ScrapedResponse]:
 
-        Args:
-            :param urls: Variable-length arguments representing the URLs to load responses from.
-            :param render_pages: Whether to render pages with JavaScript (default is False).
-            :param use_proxies: Should the request be made using a proxies
-
-        Returns:
-            Dict[str, ScrapedResponse]: A dictionary containing the loaded responses indexed by URL.
-
-        Note:
-            This method loads responses from the provided URLs. If rendering pages is enabled, it will render pages
-            with JavaScript. The method triggers a "new_responses" event with the loaded response data.
-        """
-
-        # the response method to use based on if we are rending the page or not
-        response_method = cls.get_rendered_response if render_pages \
-            else cls.get_response
+        response_method = self.get_rendered_response if self.render else self.get_response
 
         # noinspection PyTypeChecker
-        # The error indicates that the URL is considered of the wrong type for the response_method parameter,
-        # but this is not accurate. The URL is of type string, which has been confirmed through testing.
-        # This type error is likely a bug, and for now, it can be safely ignored.
-        tasks = [response_method(url, use_proxies=use_proxies) for url in urls]
+        tasks = [response_method(url) for url in urls]
 
         results = {}
         event_data = {}
-        async for result in cls._generate_responses(tasks, urls):
-            url, scraped_response = result
-
-            cls._log_response(scraped_response)
+        async for url, scraped_response in self._generate_responses(tasks, urls):
+            self._log_response(scraped_response)
 
             if scraped_response.status_code != 200:
                 continue
@@ -242,90 +183,101 @@ class ResponseLoader:
             results.update({url: scraped_response})
             event_data.update({url: scraped_response.html})
 
-        cls._event_dispatcher.sync_trigger(PEvent("new_responses", EventType.Base, data=event_data))
+        async for url, scraped_response in self._retry_urls():
+            self._log_response(scraped_response)
+
+            if scraped_response.status_code != 200:
+                continue
+
+            results.update({url: scraped_response})
+            event_data.update({url: scraped_response.html})
+
+        self._event_dispatcher.sync_trigger(PEvent("new_responses", EventType.Base, data=event_data))
 
         return results
 
-    @classmethod
-    def build_link(cls, base_url: str, href: str) -> str:
-        """
-        Build a full URL from a base URL and a relative href.
+    async def _retry_urls(self) -> AsyncGenerator[Tuple[str, ScrapedResponse], None]:
+        urls = []
+        urls_to_remove = []
+        while self._urls_to_retry:
+            for url in self._urls_to_retry:
+                if self._urls_to_retry[url] >= self.max_retires:
+                    urls_to_remove.append(url)
+                else:
+                    urls.append(url)
 
-        Args:
-            base_url (str): The base URL.
-            href (str): The relative href.
+            response_method = self.get_rendered_response if self.render else self.get_response
 
-        Returns:
-            str: The full URL.
-        """
-        if not href:
-            return ""
+            # noinspection PyTypeChecker
+            tasks = [response_method(url) for url in urls]
 
-        url = urljoin(base_url, href)
-        return cls.normalize_url(url)
+            async for url, response in self._generate_responses(tasks, urls, skip_errors=False):
+                if isinstance(response, BaseException) or response.status_code != 200:
+                    self._urls_to_retry[url] += 1
+                    self._logger.warning(f'retry failed: url {url}, times: {self._urls_to_retry[url]}')
+                else:
+                    self._logger.info(f'retry successful: url {url}')
+                    self._urls_to_retry.pop(url)
+                    yield url, response
 
-    @staticmethod
-    def get_domain(url: str) -> str:
-        return urlparse(url).netloc
+            for r_url in reversed(urls_to_remove):
+                self._urls_to_retry.pop(r_url)
 
-    @classmethod
-    async def collect_hrefs_with_elements(cls, page: Page) -> List[Locator]:
-        """
-        Collects and returns a list of Locator elements representing anchor tags (href) with specific values on a web page.
+            urls.clear()
+            urls_to_remove.clear()
 
-        Args:
-            page (Page): The Playwright Page object to search for anchor tags.
-
-        Returns:
-            List[Locator]: A list of Locator elements representing anchor tags with specific href attribute values.
-
-        """
+    async def collect_null_hrefs(self, page: Page) -> List[Locator]:
         href_elements_locator = page.locator('a[href]')
 
         hrefs_to_click = []
         for href_element in await href_elements_locator.all():
             href = await href_element.get_attribute('href')
 
-            if href in cls._hrefs_values_to_click:
+            if href in self._HREF_VALUES_TO_CLICK:
                 hrefs_to_click.append(href_element)
 
         return hrefs_to_click
 
-    @classmethod
-    def get_hrefs_from_html(cls, html: str) -> Generator[str, Any, Any]:
+    @staticmethod
+    def build_link(base_url: str, href: str) -> str:
+        if not href:
+            return ""
+
+        url = urljoin(base_url, href)
+        return ResponseLoader.normalize_url(url)
+
+    @staticmethod
+    def get_domain(url: str) -> str:
+        return urlparse(url).netloc
+
+    @staticmethod
+    def get_hrefs_from_html(html: str) -> List[str]:
         parser = HTMLParser(html)
         for a_tag in parser.css("a"):
             href = a_tag.attributes.get("href")
-            if href in cls._hrefs_values_to_click:
+            if href in ResponseLoader._HREF_VALUES_TO_CLICK:
                 continue
             yield href
 
-    @classmethod
-    async def _generate_responses(cls, tasks: List[Coroutine[None, None, ScrapedResponse]], urls: Set[str]) -> \
-            AsyncGenerator[Tuple[str, ScrapedResponse], None]:
-        """
-        Generate responses form a list of tasks and URLs.
+    async def _generate_responses(self, tasks: List[Coroutine[None, None, ScrapedResponse]], urls: Iterable[str], skip_errors: bool = True) \
+            -> AsyncGenerator[Tuple[str, ScrapedResponse], None]:
 
-        Args:
-            tasks (List[Coroutine[Any, Any, str]]): List of tasks to generate responses.
-            urls (List[str]): List of URLs corresponding to the tasks.
-
-        Yields:
-            Generator[Any, Any, Dict[str, str]]: A generator yielding dictionaries mapping URLs to their response content.
-        """
         responses = await asyncio.gather(*tasks, return_exceptions=True)
 
         for url, response_info in zip(urls, responses):
-            if isinstance(response_info, Exception):
-                cls._logger.error(f"Responses Error: {response_info.__class__}")
-                continue
+            if isinstance(response_info, Exception) or response_info.status_code != 200: # noqa
+                self._logger.error(f"Responses Error: {response_info.__class__}")
+
+                self._urls_to_retry.update({url: 0})
+                if skip_errors:
+                    continue
+
             yield url, response_info
 
-    @classmethod
-    def _log_response(cls, response: ScrapedResponse) -> None:
+    def _log_response(self, response: ScrapedResponse) -> None:
         message = f"URL={response.url}, Status={response.status_code}"
 
         if response.status_code != 200:
-            cls._logger.warning(f"Bad Response Received: {message}")
+            self._logger.warning(f"Bad Response Received: {message}")
         else:
-            cls._logger.info(f"Good Response Received: {message}")
+            self._logger.info(f"Good Response Received: {message}")
