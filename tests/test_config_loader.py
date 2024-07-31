105a1996f199a8914cbbe894ae8caf81d35093a7
Curtidor <tannermatos18@gmail.com>
2024-07-30 21:53:57 -0300

    updated file paths

--- tests/test_config_loader.py
+++ tests/test_config_loader.py
@@ -4,14 +4,14 @@ from loaders.config_loader import ConfigLoader
 
 class TestConfigLoader(unittest.TestCase):
     def test_data_order(self):
-        config = ConfigLoader('test_configs/data_order_test.json')
+        config = ConfigLoader('tests/test_configs/data_order_test.json')
 
         data_order = config.get_data_order()
 
         self.assertEqual(["Stock", "Book Name", "Book Price", "Not in data order", "element 4"], data_order)
 
     def test_data_order_with_unknown_name(self):
-        config = ConfigLoader('test_configs/data_order_test_bad_name.json')
+        config = ConfigLoader('tests/test_configs/data_order_test_bad_name.json')
 
         try:
             config.get_data_order()
